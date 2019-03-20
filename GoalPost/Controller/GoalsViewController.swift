7f1a7e4cb741cabd6411422ce717f1d370db1414
Daniel Garofalo <danielgarofalo@daniels-mbp-2.home>
2019-03-20 09:00:41 -0500

    Main VC created

---
+++ GoalPost/Controller/GoalsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GoalsViewController.swift
+//  GoalPost
+//
+//  Created by Daniel Garofalo on 3/20/19.
+//  Copyright © 2019 Daniel Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class GoalsViewController: UIViewController {
+    
+    //MARK:- Properties
+    
+    
+    
+    
+    
+    //MARK:- Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    
+    //MARK:- Buttons
+    @IBAction func goalButtonPressed(_ sender: UIButton) {
+        print("Button was pressed")
+    }
+    
+    
+    
+    
+
+}
+
